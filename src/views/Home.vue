ff267aa79d5911c0369f370df5feff56207dc198
Yimura <andreas.maerten@scarlet.be>
2021-03-29 14:56:54 +0200

    feat(Home): Dynamic content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,19 @@
     <div class="c-skillset">
       <h2>My Skillset</h2>
       <div class="c-skillset-wrapper">
-        <div class="c-skill"></div>
+        <div class="c-skill" v-bind:key="skill.name" v-for="skill in skills">
+          <div class="c-skill__head">
+            <h1>{{ skill.name }}</h1>
+            <div>
+              <span class="c-skill__tag">{{ skill.type }}</span>
+            </div>
+          </div>
+          <progress
+            v-bind:title="skill.title"
+            v-bind:value="skill.mastering"
+            max="1"
+          />
+        </div>
       </div>
     </div>
   </article>
@@ -29,19 +41,37 @@ export default {
           type: "LANGUAGE",
           name: "C#",
           title: "NOVICE",
-          mastering: 0.2,
+          mastering: 0.35,
         },
         {
           type: "LANGUAGE",
           name: "CSS / HTML",
-          title: "NOVICE",
-          mastering: 0.2,
+          title: "MODERATE",
+          mastering: 0.75,
         },
         {
           type: "LANGUAGE",
           name: "JS",
           title: "EXPERT",
           mastering: 0.95,
+        },
+        {
+          type: "LANGUAGE",
+          name: "Lua",
+          title: "MODERATE",
+          mastering: 0.45,
+        },
+        {
+          type: "LANGUAGE",
+          name: "PHP",
+          title: "MODERATE",
+          mastering: 0.55,
+        },
+        {
+          type: "LANGUAGE",
+          name: "Python",
+          title: "MODERATE",
+          mastering: 0.65,
         },
       ],
     };
@@ -60,5 +90,36 @@ export default {
 
 .c-skillset {
   margin: var(--baseline-m) var(--baseline-xl);
+}
+
+.c-skillset-wrapper {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  margin: 0 calc(-1 * var(--baseline-xl));
+}
+
+.c-skill {
+  border: 1px solid var(--gray-suit);
+  border-radius: calc(var(--baseline) / 2);
+  margin: 0 var(--baseline-xs) var(--baseline-s);
+  min-width: 300px;
+  padding: var(--baseline) var(--baseline-xs);
+}
+
+.c-skill__head {
+  display: flex;
+}
+
+.c-skill__head > h1 {
+  flex-grow: 1;
+}
+
+.c-skill__tag {
+  background-color: var(--gray-suit);
+  border-radius: var(--baseline-xs);
+  color: var(--steel-gray);
+  font-size: 14px;
+  font-weight: bold;
+  padding: 2px 8px;
 }
 </style>
