2663c55304078b370c57c8ede85648829969f409
Yimura <andreas.maerten@scarlet.be>
2021-03-30 11:54:33 +0200

    feat(Package): Added font-awesome icons

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// Font Awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faDiscord,
+  faGithub,
+  faSteam,
+  faTwitch,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faDiscord, faGithub, faSteam, faTwitch, faTwitter);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
